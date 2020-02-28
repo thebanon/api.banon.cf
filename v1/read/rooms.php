b413299fe18abb083f827833afe714ba42eadd40
Tim Banon <izmbanon@gmail.com>
2020-02-27 20:24:20 -0500

    Deleted unneccessary files

--- v1/read/rooms.php
+++
@@ -1,5 +0,0 @@
-<?php
-$query = mysqli_query($con,'SELECT * FROM rooms');
-$key=0; while($row = mysqli_fetch_assoc($query)) { $data[$key] = $row; $key++; }
-mysqli_close($con);
-?>
--- www/cache/README.md
+++
@@ -1,3 +0,0 @@
-# MallZones JSON Database  
-Works well with TaffyDB & AJAX  
-<a href="https://mallzones.com" target="_blank">MallZones Database API</a>
