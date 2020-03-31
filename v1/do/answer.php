199f48ab083507027ac9a3466912470ea030cc1b
Tim Banon <izmbanon@gmail.com>
2020-03-31 18:02:18 -0400

    Update answer.php

--- v1/do/answer.php
+++ v1/do/answer.php
@@ -1,12 +1,13 @@
 <?php
 $questions = array(
   "How many licks does it take to get to the center of a tootsie pop" => "The world may never know...",
-  "If a tree fell and nobody was there to hear it does it make a sound?" => "The world may never know..."
+  "If a tree fell and nobody was there to hear it does it make a sound" => "Depends if somebody was there with them..."
 );
 $count = count($questions);
-
+$question = urldecode($ep[0]);
 $last_joke = $count === 1 ? 0 : count($questions)-1;
-$response = $questions[urldecode($ep[0])];
+$response = $questions[$question];
+$data['question'] = $question;
 $data['response'] = $response;
 $data['questions'] = $questions;
 $data['ep'] = $ep;
