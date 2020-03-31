db1a36aba17a12ce8e931cfbf3c0c3f9a822c38e
Tim Banon <izmbanon@gmail.com>
2020-03-31 01:19:13 -0400

    Update buttchug.php

--- v1/do/buttchug.php
+++ v1/do/buttchug.php
@@ -4,6 +4,6 @@ $count = count($ep);
 if($count==2) { $response = "buttchugs ".$ep[1]."!!!"; } 
 else { $response = "chugs booty!!!"; }
 $data["endpoint"] = $ep;
-$data["results"] = $response;
+$data["response"] = $response;
 $data["count"] = $count;
 ?>
