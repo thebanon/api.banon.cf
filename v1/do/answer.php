ea1f0bec4bad9b26cf817b7de1b433eaa97ddf4e
Tim Banon <izmbanon@gmail.com>
2020-03-31 17:59:55 -0400

    Update answer.php

--- v1/do/answer.php
+++ v1/do/answer.php
@@ -1,12 +1,13 @@
 <?php
-$jokes = array(
-  "How many licks does it take to get to the center of a tootsie pop" => "The world may never know..."
+$questions = array(
+  "How many licks does it take to get to the center of a tootsie pop" => "The world may never know...",
+  "If a tree fell and nobody was there to hear it does it make a sound?" => "The world may never know..."
 );
-$count = count($jokes);
+$count = count($questions);
 
-$last_joke = $count === 1 ? 0 : count($jokes)-1;
-$response = $jokes[urldecode($ep[0])].' 🥁 badum tssst';
+$last_joke = $count === 1 ? 0 : count($questions)-1;
+$response = $questions[urldecode($ep[0])];
 $data['response'] = $response;
-$data['jokes'] = $jokes;
+$data['questions'] = $questions;
 $data['ep'] = $ep;
 ?>
