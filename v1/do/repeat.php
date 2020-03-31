58c08f9a2c68c36b029c102bf2c549fba1c0a8e2
Tim Banon <izmbanon@gmail.com>
2020-03-31 14:15:38 -0400

    Create repeat.php

---
+++ v1/do/repeat.php
@@ -0,0 +1,28 @@
+<?php
+$ep = array_slice($_GET,2);
+$count = count($ep);
+
+$short = $_GET[3];
+$short === "joy" ? $emoji = "😂" : null;
+$short === "finger" ? $emoji = "🖕" : null;
+//$data["GET"] = $emoji;
+$response = '';
+if($count===3 && $ep[2]) { 
+
+    $data['int'] = (int)$ep[2];
+    if($data['int']>0) {
+    $x=0; while($x < $data['int']) {
+        $response .= $ep[1].' ';
+        $x++;
+    }
+    } else {        
+        $response = $emoji;   
+    }
+} else {
+    $response = $emoji;    
+}
+
+$data["response"] = $response;
+$data["endpoint"] = $ep;
+$data["count"] = $count;
+?>
