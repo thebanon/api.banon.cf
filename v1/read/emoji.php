91b58f2764f3d6fced7c6d963889741040fccac7
Tim Banon <izmbanon@gmail.com>
2020-03-31 14:01:18 -0400

    Update emoji.php

--- v1/read/emoji.php
+++ v1/read/emoji.php
@@ -1,6 +1,22 @@
 <?php
+$ep = array_slice($_GET,2);
+$count = count($ep);
+
 $short = $_GET[3];
 $short === "joy" ? $emoji = "😂" : null;
 $data["GET"] = $emoji;
+
+if($count===3 && $ep[2]) { 
+
+    $data['int'] = (int)$ep[2];
+    $x=0; while($x < $data['int']) {
+        $data['what'] = $x;
+        $emoji .= $emoji.' ';
+        $x++;
+    }
+}
+
 $data["response"] = $emoji;
+$data["endpoint"] = $ep;
+$data["count"] = $count;
 ?>
