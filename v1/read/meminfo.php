53c5a38274e37897a4fbcd88325f00358440d3b4
Tim Banon <izmbanon@gmail.com>
2020-03-31 02:09:53 -0400

    Updated framework

--- v1/read/meminfo.php
+++ v1/read/meminfo.php
@@ -11,5 +11,15 @@
   fclose($fh);
 
 
-  $data["results"] = $data["response"] = "$mem kB RAM found";
+  $data["results"] = $data["response"] = $mem." kB RAM found";
+  $data["formatted"] = formatBytes(62914752,2);
+
+  function findBytes($bytes, $to, $decimal_places = 1) {
+      $formulas = array(
+          'K' => number_format($bytes / 1024, $decimal_places),
+          'M' => number_format($bytes / 1048576, $decimal_places),
+          'G' => number_format($bytes / 1073741824, $decimal_places)
+      );
+      return isset($formulas[$to]) ? $formulas[$to] : 0;
+  }
 ?>
