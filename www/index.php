84f1fa4bbbf1ee3772417b3edef4ca88d1172861
Tim Banon <izmbanon@gmail.com>
2020-04-08 19:39:52 -0400

    Debugged framework

--- www/index.php
+++ www/index.php
@@ -7,7 +7,7 @@ $_GET = explode('/',trim($_SERVER['REQUEST_URI'],'/'));
 $query = array_slice($_GET,2);
 $params = count($query);
 $endpoints = ['create', 'read', 'update', 'delete'];
-$include = in_array($_GET[1], $endpoints) ? '../'.$_GET[0].'/'.$_GET[1].'/index.php' : '../404.json';
+$include = count($_GET)>1 && in_array($_GET[1], $endpoints) ? '../'.$_GET[0].'/'.$_GET[1].'/index.php' : '../404.json';
 include($include);
 print_r(json_encode($data, JSON_PRETTY_PRINT));
 unset($data);
