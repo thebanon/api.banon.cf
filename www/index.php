2e99d2eea47d7dd475fc1173e44a0aa7c2c72558
Tim Banon <izmbanon@gmail.com>
2020-04-09 01:26:49 -0400

    Updated API

--- www/index.php
+++ www/index.php
@@ -3,12 +3,15 @@ session_start();
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 $data = [];
-$_GET = explode('/',trim($_SERVER['REQUEST_URI'],'/'));
+$data['get'] = $_GET = explode('/',trim($_SERVER['REQUEST_URI'],'/'));
 $query = array_slice($_GET,2);
 $params = count($query);
 $endpoints = ['create', 'read', 'update', 'delete', 'do', 'is', 'play'];
-$include = count($_GET)>1 && in_array($_GET[1], $endpoints) ? '../'.$_GET[0].'/'.$_GET[1].'/index.php' : '../404.json';
-include($include);
+if(count($_GET)>1 && in_array($_GET[1], $endpoints)) {    
+    include('../'.$_GET[0].'/'.$_GET[1].'/index.php');
+} else {    
+    //$data["error"] = 404;
+}
 print_r(json_encode($data, JSON_PRETTY_PRINT));
 unset($data);
 ?>
